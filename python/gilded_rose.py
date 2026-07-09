f6b757cc293fcf65935966e5141145ee4260bc40
Matei Stefanica <matei.stefanica@softwire.com>
2026-07-09 16:52:43 +0300

    removed magic numbers and added a 'modify_quality' function

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,84 +1,101 @@
-# -*- coding: utf-8 -*-
+from enum import StrEnum
+
+BASE_DECREASE = -1
+BASE_INCREASE = 1
+MILK_DECAYING_FACTOR = 1.1
+BASE_UPPER_QUALITY_BOUND = 50
+BASE_LOWER_QUALITY_BOUND = 0
+BRIE_DECAY_MULTIPLIER = 2
+TICKETS_FIRST_INCREASE_THRESHOLD = 10
+TICKETS_SECOND_INCREASE_THRESHOLD = 5
+TICKETS_FIRST_QUALITY_MODIFIER = 2
+TICKETS_SECOND_QUALITY_MODIFIER = 3
+CONJURED_MULTIPLIER = 2
+PAST_SELLIN_MULTIPLIER = 2
+DAILY_DECREMENT = 1
+BAD_ITEM_LOWER_BOUND = -10
+MILK_LOWER_BOUND = -50
+
+class ProductName(StrEnum):
+    BASE_ITEM = "greatItem"
+    SULFURAS = "Sulfuras, Hand of Ragnaros"
+    BRIE = "Aged Brie"
+    TICKETS = "Backstage passes to a TAFKAL80ETC concert"
+    CONJURED = "Conjured"
+    BAD = "Bad Item"
+    MILK = "Milk"
 
 class GildedRose(object):
-
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
-        BASE_DECREASE = 1
-        BASE_INCREASE = 1
-        MILK_DECAYING_FACTOR = 1.1
-
         for item in self.items:
             match item.name:
                 case "Aged Brie":
-                    if item.quality < 50:
-                        if item.sell_in > 0:
-                            item.quality += BASE_INCREASE
+                    if item.quality < BASE_UPPER_QUALITY_BOUND:
+                        if item.sell_in > BASE_LOWER_QUALITY_BOUND:
+                            item.modify_quality(BASE_INCREASE)
                         else:
-                            item.quality = item.quality + BASE_INCREASE * 2
+                            item.modify_quality(BRIE_DECAY_MULTIPLIER * BASE_INCREASE)
                 case "Sulfuras, Hand of Ragnaros":
                     continue
                 case "Backstage passes to a TAFKAL80ETC concert":
-                    if item.sell_in <= 0:
-                        item.quality = 0
+                    if item.sell_in <= BASE_LOWER_QUALITY_BOUND:
+                        item.quality = BASE_LOWER_QUALITY_BOUND
                     else:
-                        if item.quality < 50:
-                            if item.sell_in > 10:
-                                item.quality += BASE_INCREASE
-                            elif item.sell_in > 5:
-                                item.quality = item.quality + 2
-                            elif item.sell_in > 0:
-                                item.quality = item.quality + 3
+                        if item.quality < BASE_UPPER_QUALITY_BOUND:
+                            if item.sell_in > TICKETS_FIRST_INCREASE_THRESHOLD:
+                                item.modify_quality(BASE_INCREASE)
+                            elif item.sell_in > TICKETS_SECOND_INCREASE_THRESHOLD:
+                                item.modify_quality(TICKETS_FIRST_QUALITY_MODIFIER)
+                            elif item.sell_in > BASE_LOWER_QUALITY_BOUND:
+                                item.modify_quality(TICKETS_SECOND_QUALITY_MODIFIER)
                 case "Conjured":
-                    if item.sell_in > 0:
-                        item.quality -= 2 * BASE_DECREASE
+                    if item.sell_in > BASE_LOWER_QUALITY_BOUND:
+                        item.modify_quality(CONJURED_MULTIPLIER * BASE_DECREASE)
                     else:
-                        item.quality -= 4 * BASE_DECREASE
+                        item.modify_quality(CONJURED_MULTIPLIER * PAST_SELLIN_MULTIPLIER * BASE_DECREASE)
                 case "Bad Item":
-                    if item.sell_in > 0:
-                        item.quality -= BASE_DECREASE
+                    if item.sell_in > BASE_LOWER_QUALITY_BOUND:
+                        item.modify_quality(BASE_DECREASE)
                     else:
-                        item.quality -= 2 * BASE_DECREASE
-                    item.bound_quality_custom(-10, 50)
-                    item.sell_in -= 1   
+                        item.modify_quality(PAST_SELLIN_MULTIPLIER * BASE_DECREASE)
+                    item.bound_quality_custom(BAD_ITEM_LOWER_BOUND, BASE_UPPER_QUALITY_BOUND)
+                    item.sell_in -= DAILY_DECREMENT   
                     continue
                 case "Milk":
-                    if item.sell_in > 0:
-                        item.quality -= BASE_DECREASE
+                    if item.sell_in > BASE_LOWER_QUALITY_BOUND:
+                        item.modify_quality(BASE_DECREASE)
                     else:
-                        item.quality -= int(BASE_DECREASE * pow(MILK_DECAYING_FACTOR, abs(item.sell_in)))
-                    item.bound_quality_custom(-50, 50)
-                    item.sell_in -= 1   
+                        item.quality -= int(-BASE_DECREASE * pow(MILK_DECAYING_FACTOR, abs(item.sell_in)))
+                    item.bound_quality_custom(MILK_LOWER_BOUND, BASE_UPPER_QUALITY_BOUND)
+                    item.sell_in -= DAILY_DECREMENT   
                     continue
                 case _:
-                    if item.quality > 0:
-                        if item.sell_in > 0:
-                            item.quality -= BASE_DECREASE
+                    if item.quality > BASE_LOWER_QUALITY_BOUND:
+                        if item.sell_in > BASE_LOWER_QUALITY_BOUND:
+                            item.modify_quality(BASE_DECREASE)
                         else:
-                            item.quality -= 2 * BASE_DECREASE
-            item.bound_quality()
-            item.sell_in -= 1   
+                            item.modify_quality(PAST_SELLIN_MULTIPLIER * BASE_DECREASE)
+            item.bound_quality_custom(BASE_LOWER_QUALITY_BOUND, BASE_UPPER_QUALITY_BOUND)
+            item.sell_in -= DAILY_DECREMENT   
 
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
-        self.bound_quality()
-
-    def bound_quality(self):
-        if self.quality < 0:
-            self.quality = 0
-        if self.quality > 50:
-            self.quality = 50
+        self.bound_quality_custom(BASE_LOWER_QUALITY_BOUND, BASE_UPPER_QUALITY_BOUND)
 
     def bound_quality_custom(self, lowerBound, upperBound):
         if self.quality < lowerBound:
             self.quality = lowerBound
         if self.quality > upperBound:
             self.quality = upperBound
+
+    def modify_quality(self, value):
+        self.quality += value
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -1,175 +1,156 @@
 # -*- coding: utf-8 -*-
 import unittest
-from enum import Enum
-
-
+from gilded_rose import ProductName
 from gilded_rose import Item, GildedRose
-
-class Object_type(str, Enum):
-    BASE_ITEM = "greatItem"
-    SULFURAS = "Sulfuras, Hand of Ragnaros"
-    BRIE = "Aged Brie"
-    TICKETS = "Backstage passes to a TAFKAL80ETC concert"
-    CONJURED = "Conjured"
-    BAD = "Bad Item"
-    MILK = "Milk"
-
-
 
 class GildedRoseTest(unittest.TestCase):
     def test_name(self):
-        items = [Item(Object_type.BASE_ITEM, 0, 0)]
+        items = [Item(ProductName.BASE_ITEM, 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual(Object_type.BASE_ITEM, items[0].name)
+        self.assertEqual(ProductName.BASE_ITEM, items[0].name)
 
     def test_updated_properties_normal_item_before_selling(self):
-        items = [Item(Object_type.BASE_ITEM, 10, 10)]
+        items = [Item(ProductName.BASE_ITEM, 10, 10)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(9, items[0].quality)
         self.assertEqual(9, items[0].sell_in)
 
     def test_updated_properties_normal_item_after_sellin_expires(self):
-        items = [Item(Object_type.BASE_ITEM, 0, 10)]
+        items = [Item(ProductName.BASE_ITEM, 0, 10)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(8, items[0].quality)
         self.assertEqual(-1, items[0].sell_in)
 
     def test_quality_never_negative(self):
-        items = [Item(Object_type.BASE_ITEM, 0, 0)]
+        items = [Item(ProductName.BASE_ITEM, 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(0, items[0].quality)
         self.assertEqual(-1, items[0].sell_in)
 
     def test_quality_increase_for_aged_brie_before_sellin(self):
-        items = [Item(Object_type.BRIE, 1, 10)]
+        items = [Item(ProductName.BRIE, 1, 10)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(11, items[0].quality)
         self.assertEqual(0, items[0].sell_in)
 
     def test_quality_increase_for_aged_brie_after_sellin(self):
-        items = [Item(Object_type.BRIE, 0, 20)]
+        items = [Item(ProductName.BRIE, 0, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(22, items[0].quality)
         self.assertEqual(-1, items[0].sell_in)
 
     def test_quality_not_greater_than_50(self):
-        items = [Item(Object_type.BRIE, 0, 50)]
+        items = [Item(ProductName.BRIE, 0, 50)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(50, items[0].quality)
         self.assertEqual(-1, items[0].sell_in)
 
     def test_sulfuras_never_changes_properties(self):
-        items = [Item(Object_type.SULFURAS, 12, 23)]
+        items = [Item(ProductName.SULFURAS, 12, 23)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(23, items[0].quality)
         self.assertEqual(12, items[0].sell_in)
 
     def test_backstage_passes_with_more_than_5_days_but_less_than_10(self):
-        items = [Item(Object_type.TICKETS, 12, 23)]
+        items = [Item(ProductName.TICKETS, 12, 23)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(24, items[0].quality)
         self.assertEqual(11, items[0].sell_in)
 
     def test_backstage_passes_with_less_than_10_days(self):
-        items = [Item(Object_type.TICKETS, 8, 23)]
+        items = [Item(ProductName.TICKETS, 8, 23)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(25, items[0].quality)
         self.assertEqual(7, items[0].sell_in)
 
     def test_backstage_passes_with_less_than_5_days(self):
-        items = [Item(Object_type.TICKETS, 4, 23)]
+        items = [Item(ProductName.TICKETS, 4, 23)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(26, items[0].quality)
         self.assertEqual(3, items[0].sell_in)
 
     def test_backstage_passes_day_of_the_concert(self):
-        items = [Item(Object_type.TICKETS, 0, 23)]
+        items = [Item(ProductName.TICKETS, 0, 23)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(0, items[0].quality)
         self.assertEqual(-1, items[0].sell_in)
 
     def test_backstage_passes_day_after_concert(self):
-        items = [Item(Object_type.TICKETS, -2, 23)]
+        items = [Item(ProductName.TICKETS, -2, 23)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(0, items[0].quality)
         self.assertEqual(-3, items[0].sell_in)
 
     def test_repr(self):
-        items = [Item(Object_type.BASE_ITEM, 10, 10)]
+        items = [Item(ProductName.BASE_ITEM, 10, 10)]
         representation = items[0].__repr__()
-        self.assertEqual(representation, str(Object_type.BASE_ITEM) + ", 10, 10")
+        self.assertEqual(representation, str(ProductName.BASE_ITEM) + ", 10, 10")
 
     def test_initial_quality_cant_be_over_50(self):
-        items = [Item(Object_type.BASE_ITEM, 5, 58)]
+        items = [Item(ProductName.BASE_ITEM, 5, 58)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(49, items[0].quality)
         self.assertEqual(4, items[0].sell_in)
 
     def test_negative_sellin_from_the_start(self):
-        items = [Item(Object_type.BASE_ITEM, -3, 20)]
+        items = [Item(ProductName.BASE_ITEM, -3, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(18, items[0].quality)
         self.assertEqual(-4, items[0].sell_in)
 
     def test__brie_quality_upper_bound(self):
-        items = [Item(Object_type.BRIE, 0, 49)]
+        items = [Item(ProductName.BRIE, 0, 49)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(50, items[0].quality)
         self.assertEqual(-1, items[0].sell_in)
 
-    def test__backstage_tickets_quality_uppber_bound_more_than_5_but_less_than_10(self):
-        items = [Item(Object_type.TICKETS, 8, 49)]
+    def test__backstage_tickets_quality_uppber_bound(self):
+        items = [Item(ProductName.TICKETS, 8, 49)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(50, items[0].quality)
         self.assertEqual(7, items[0].sell_in)
 
-    def test__backstage_tickets_quality_upper_bound_less_than_5_days_left(self):
-        items = [Item(Object_type.TICKETS, 2, 49)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual(50, items[0].quality)
-        self.assertEqual(1, items[0].sell_in)
 
     def test_quality_cant_be_negative_when_creating_the_object(self):
-        items = [Item(Object_type.BASE_ITEM, 20, -3)]
+        items = [Item(ProductName.BASE_ITEM, 20, -3)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(0, items[0].quality)
         self.assertEqual(19, items[0].sell_in)
 
-    def test_conjured(self):
-        items = [Item(Object_type.CONJURED, 5, 10)]
+    def test_conjured_doubles_quality_decrease(self):
+        items = [Item(ProductName.CONJURED, 5, 10)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(8, items[0].quality)
         self.assertEqual(4, items[0].sell_in)
 
     def test_bad_item(self):
-        items = [Item(Object_type.BAD, 2, 0)]
+        items = [Item(ProductName.BAD, 2, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(-1, items[0].quality)
         self.assertEqual(1, items[0].sell_in)
 
     def test_bad_item_lower_bound(self):
-        items = [Item(Object_type.BAD, 0, 0)]
+        items = [Item(ProductName.BAD, 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         gilded_rose.update_quality()
@@ -181,7 +162,7 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(-6, items[0].sell_in)
 
     def test_milk_lower_bound(self):
-        items = [Item(Object_type.MILK, 0, 0)]
+        items = [Item(ProductName.MILK, 0, 0)]
         gilded_rose = GildedRose(items)
         for i in range(30):
             gilded_rose.update_quality()
